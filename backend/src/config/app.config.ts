2acf74723440c658e2b8a221b0e9fde8bcdc5565
Moein <moein.azizi1997@gmail.com>
2026-07-27 20:13:22 +0330

    Implemented initial backend server with /health route for health checking

---
+++ backend/src/config/app.config.ts
@@ -0,0 +1,16 @@
+import { getEnv } from "../common/utils/get-env";
+
+const appConfig = ()=>({
+    NODE_ENV : getEnv("NODE_ENV","development"),
+    APP_ORIGIN : getEnv("APP_ORIGIN","http://localhost:3000"),
+    PORT : getEnv("PORT","5000"),
+    BASE_PATH : getEnv("BASE_PATH","/api/v1"),
+    JWT : {
+        JWT_ACCESS_TOKEN_SECRET: getEnv("JWT_ACCESS_TOKEN_SECRET"),
+        JWT_ACCESS_TOKEN_EXPIRES_IN : getEnv("JWT_ACCESS_TOKEN_EXPIRES_IN",'15m'),
+        JWT_REFRESH_TOKEN_SECRET : getEnv("JWT_REFRESH_TOKEN_SECRET"),
+        JWT_REFRESH_TOKEN_EXPIRES_IN : getEnv("JWT_REFRESH_TOKEN_EXPIRES_IN","7d")
+    }
+});
+
+export const config = appConfig();
---
+++ backend/src/index.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import cors from "cors"
+import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import { config } from "./config/app.config";
+
+const BASE_PATH = config.BASE_PATH;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cors({
+    credentials : true,
+    origin : config.APP_ORIGIN
+}));
+app.use(cookieParser());
+
+app.get("/health",(req:Request,res:Response)=>{
+    return res.status(200).json({
+        success : true,
+        message : "healthy"
+    })
+});
+
+app.listen(config.PORT,async ()=>{
+    console.log(`Server is listening on port ${config.PORT} in ${config.NODE_ENV}`)
+})
