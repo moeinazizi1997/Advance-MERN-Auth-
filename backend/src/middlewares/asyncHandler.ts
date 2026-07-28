faf191f8e26f0f2dcdc20fa0f794f16c44d65a34
Moein <moein.azizi1997@gmail.com>
2026-07-28 21:14:42 +0330

    modified asyncHandler for better performance and also errorHandler middleware for handling validation error of zod

--- backend/src/middlewares/asyncHandler.ts
+++ backend/src/middlewares/asyncHandler.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from "express"
 
-type AsyncControllerType = (req:Request,res:Response,next:NextFunction)=> Promise<any>
+type AsyncControllerType = (req:Request,res:Response,next:NextFunction)=> Promise<void | Response>
 
 export const asyncHandler = (controller:AsyncControllerType):AsyncControllerType=>{
     return async (req,res,next)=>{
         try{
-            await controller(req,res,next)
+            return await controller(req,res,next)
         }catch(err){
             next(err);
         }
--- backend/src/middlewares/errorHandler.middleware.ts
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -1,15 +1,23 @@
 import { ErrorRequestHandler} from "express";
 import { HTTPSTATUS } from "../config/http.config";
 import { AppError } from "../common/utils/AppError";
+import { ZodError } from "zod";
 
 const errorHandlerMiddleware : ErrorRequestHandler = (err,req,res,next): any=>{
-    console.log(`Error occured on PATH: ${req.path} and error is:`,err);
+    console.log(`Error occured on PATH: ${req.path} and error is:`,err.message);
 
     if(err instanceof AppError){
         return res.status(err.statusCode).json({
             message : err.message,
             errorCode : err.errorCode
         })
+    };
+
+    if (err instanceof ZodError) {
+        return res.status(HTTPSTATUS.BAD_REQUEST).json({
+        message: "Validation failed",
+        errors: err.flatten().fieldErrors,
+        });
     }
 
     return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
